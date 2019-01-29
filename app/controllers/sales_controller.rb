8d7e8a45ff0b507a4a0aa6a6049219a0f8984f77
Sebastian Poblete <sebapobletec@gmail.com>
2019-01-29 00:22:10 -0300

    Change controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -11,14 +11,14 @@ class SalesController < ApplicationController
   def create
     @sale = Sale.new(sales_params)
     @sale.total = @sale.value * (100 - @sale.discount) / 100
-    if @sale.tax == 0
-      @sale.total = @sale.total * 1.19
-      @sale.tax = 19
-    else
-      @sale.tax = 0
-    end
-    @sale.save
-    redirect_to sales_path
+      if @sale.tax == 0
+        @sale.total = @sale.total * 1.19
+        @sale.tax = 19
+      else
+        @sale.tax = 0
+      end
+      @sale.save
+      redirect_to sales_path
   end
 
   private
